2696f1327dc9f5a20fa082398cbd65ec55e68e62
Robson Liebke <liebke@media-interactive.de>
2020-12-04 20:55:07 +0100

    use angular animations for sidebar

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,16 +1,30 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(-80%,0,0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class LayoutComponent implements OnInit {
 
-  isOpened: boolean = true;
+  sidebarStatus: string = 'out';
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 }
