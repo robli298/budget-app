ed45c132efea7ab288e954dce4140b937c8d42d4
Robson Liebke <liebke@media-interactive.de>
2020-12-16 00:18:42 +0100

    add navbar static data

--- src/app/shared/models/layout.model.ts
+++ src/app/shared/models/layout.model.ts
@@ -1,5 +1,10 @@
 
 export interface LayoutModel {
+    navbar: {
+        alerts: [
+            {class: string}
+        ]
+    }
     footer: { version: number, socialMedias: Array<{ url: string, iconClass: string }> }
 }
 
