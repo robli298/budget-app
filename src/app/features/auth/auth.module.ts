cf3e7200c5207208c8c24cefa09bb490b18ae9da
Robson Liebke <liebke@media-interactive.de>
2021-01-28 18:44:26 +0100

    improve folders structue

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { AuthRoutingModule } from "./auth-routing.module";
-import { LoginComponent } from "./login/login.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @NgModule({
     declarations: [
